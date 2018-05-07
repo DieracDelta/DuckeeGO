eaaa7cd81fc4f5d9ca374a973427d4fc1f68e58d
Grace Yin <graceyin1218@gmail.com>
2018-05-07 01:24:12 -0400

    And he waddled away... (implemented more stuff in concolicInt and concolicBool)

--- src/concolicTypes/concolicInt.go
+++ src/concolicTypes/concolicInt.go
@@ -18,8 +18,9 @@ import "github.com/aclements/go-z3/z3"
 // }
 
 type ConcolicInt struct {
-	Value int
-	Sym   symInt.SymInt
+	Value     int
+	Sym       symTypes.SymInt
+  Constant  bool
 }
 
 func (self ConcolicInt) equals(o interface{}) ConcolicBool {
@@ -30,20 +31,90 @@ func (self ConcolicInt) equals(o interface{}) ConcolicBool {
 	case ConcolicInt:
 		res := self.Value == ConcolicInt(o).Value
 	default:
-		return ConcolicBool(false)
+    return ConcolicBool{Value: false, Sym: ...}
 	}
   return ConcolicBool{Value:res, Sym: ... }
+}
+
+func (self ConcolicInt) notEquals(o interface{}) ConcolicBool {
+  return !self.equals(o)
+}
+
+func (self ConcolicInt) lt(o interface{}) ConcolicBool {
+  switch o.(type) {
+  case int:
+    res := self.Value < int(o)
+    sym := self.Sym.SymIntZ3Expr().LT(z3.Int(int(o)))
+  case ConcolicInt:
+    res := self.Value < ConcolicInt(o).Value
+    sym := self.Sym.SymIntZ3Expr().LT(ConcolicInt(o).Sym.SymIntZ3Expr())
+  default:
+    reportError("cannot compare with < : incompatible types", self, o)
+    // do something?
+  }
+  return ConcolicBool{Value: res, Sym: sym}
+}
+
+func (self ConcolicInt) le(o interface{}) ConcolicBool {
+  switch o.(type) {
+  case int:
+    res := self.Value <= int(o)
+    sym := self.Sym.SymIntZ3Expr().LE(z3.Int(int(o)))
+  case ConcolicInt:
+    res := self.Value <= ConcolicInt(o).Value
+    sym := self.Sym.SymIntZ3Expr().LE(ConcolicInt(o).Sym.SymIntZ3Expr())
+  default:
+    reportError("cannot compare with <= : incompatible types", self, o)
+    // do something?
+  }
+  return ConcolicBool{Value: res, Sym: sym}
+}
+
+func (self ConcolicInt) gt(o interface{}) ConcolicBool {
+  switch o.(type) {
+  case int:
+    res := self.Value > int(o)
+    sym := self.Sym.SymIntZ3Expr().GT(z3.Int(int(o)))
+  case ConcolicInt:
+    res := self.Value > ConcolicInt(o).Value
+    sym := self.Sym.SymIntZ3Expr().GT(ConcolicInt(o).Sym.SymIntZ3Expr())
+  default:
+    reportError("cannot compare with > : incompatible types", self, o)
+    // do something?
+  }
+  return ConcolicBool{Value: res, Sym: sym}
+}
+
+func (self ConcolicInt) ge(o interface{}) ConcolicBool {
+  switch o.(type) {
+  case int:
+    res := self.Value >= int(o)
+    sym := self.Sym.SymIntZ3Expr().GE(z3.Int(int(o)))
+  case ConcolicInt:
+    res := self.Value >= ConcolicInt(o).Value
+    sym := self.Sym.SymIntZ3Expr().GE(ConcolicInt(o).Sym.SymIntZ3Expr())
+  default:
+    reportError("cannot compare with >= : incompatible types", self, o)
+    // do something?
+  }
+  return ConcolicBool{Value: res, Sym: sym}
 }
 
 func (self ConcolicInt) add(o interface{}) ConcolicInt {
   switch o.(type) {
   case int:
     res := self.Value + int(o)
+    sym := self.Sym.SymIntz3Expr().Add(z3.Int(int(o)))
   case ConcolicInt:
     res := self.Value + ConcolicInt(o).Value
+    sym := self.Sym.SymIntZ3Expr().Add(ConcolicInt(o).Sym.SymIntZ3Expr())
   default:
     // something went very wrong.
     return nil
   }
   return ConcolicInt{Value:res, Sym:sym}
 }
+
+
+
+
--- src/symTypes/symBool.go
+++ src/symTypes/symBool.go
@@ -1,1 +1,28 @@
 package symTypes
+
+import "github.com/aclements/go-z3/z3"
+import "reflect"
+import "hash/fnv"
+
+type SymBool struct {
+  id bool
+}
+
+// TODO return concolic bool?
+func (self *SymBool) SymBoolEquals(o interface{}) bool {
+  if reflect.TypeOf(o) != SymBool {
+    return false
+  }
+  return self.id == (SymBool(o)).id
+}
+
+func (self *SymBool) SymBoolZ3Expr() z3.Bool {
+  return z3.Bool(self.id)
+}
+
+func (self *SymBool) SymBoolHash() int {
+  h := fnv.New32a()
+  h.Write([]byte(self.id))
+  return h.Sum32()
+}
+

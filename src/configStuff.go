1c0a4ec0e98ba5dc68d6209e96d01a94dd1ea4e1
Justin Restivo <jrestivo@mit.edu>
2018-05-09 22:44:51 -0400

    waddle waddle

---
+++ src/configStuff.go
@@ -0,0 +1,23 @@
+package main
+
+// import "golang.org/x/tools/go/ast/astutil"
+import "go/ast"
+
+type ConfigData struct {
+	ConfigData  []GoFile `json:"Data"`
+	ProjectPath string   `json:"ProjectPath"`
+	Package     string   `json:"Package"`
+}
+
+type GoFile struct {
+	FilePath  string       `json:"RelFilePath"`
+	Functions []GoFunction `json:"Functions"`
+}
+
+type GoFunction struct {
+	Name   string   `json:"Name"`
+	Object string   `json:"Object"`
+	Input  []string `json:"Input"`
+	Output []string `json:"Output"`
+	Node   *ast.Node
+}

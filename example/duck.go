0c932775744c50b3daef9386112a3af73cce1922
Justin Restivo <jrestivo@mit.edu>
2018-05-09 22:57:30 -0400

    waddle waddle

---
+++ example/duck.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Quack(a bool) {
+	fmt.Printf("bruh %s\r\n", a)
+}
---
+++ example/memey.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Danck(a bool, b int, c int) bool {
+	d := 55
+	f := 6969
+	g := f + d + 8
+	g++
+	g--
+
+	var i int
+	var j int
+
+	var k bool
+
+	k = !!false
+
+	q := true
+	_ = q
+
+	i = 5
+
+	i++
+
+	j = 69
+
+	i -= (420 + j) * 69
+
+	if h := 5; h+i+j != 420 {
+		fmt.Printf("mother fucekr")
+
+	} else if i == 5 {
+
+	} else {
+		fmt.Printf("mickey ds")
+
+	}
+
+	return k && (c) == 69 && q
+}
